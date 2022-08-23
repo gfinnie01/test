9656bcbb2dd4fba2de3d227b03a7fd213c2a94ef
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-08-22 22:33:24 -0400

    just finnished reducing my variables and added dispaly questions into my quiz function dont know why all the functions are still on the page but i will do more research and get back to it tommorow

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,9 @@ var startBtn = document.querySelector("#start button");
 var submitScoreBtn = document.querySelector("#start button");
 var quizTitle = document.querySelector("#quiz title");
 var questionEm = document.querySelector("#questions");
-var questions = ("First");
-var questions = ("second");
-var questions = ("third");
-var questions = ("fourth");
-var state = ("start");
-var state = ("quiz");
-var state = ("end");
-
+var questions = ["First", "second", "third", "fourth"];
+var state = ['start', 'quiz', 'end' ];
+var cursor = 0;
 
 function displayState() {
     if( state == 'start' ) {
@@ -24,16 +19,17 @@ function displayState() {
 
 function displayState() {
     if( state == 'quiz' ) {
-        startEm.style.display = 'block';
-        quizEm.style.display = 'none';
+        startEm.style.display = 'none';
+        quizEm.style.display = 'block';
         endEm.style.display = 'none';
+        displayQuestion();
     }
 }
 
 function displayState() {
     if( state == 'end' ) {
-        startEm.style.display = 'block';
+        startEm.style.display = 'none';
         quizEm.style.display = 'none';
-        endEm.style.display = 'none';
+        endEm.style.display = 'block';
     }
 }
