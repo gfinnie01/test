50fbdbc232465703a658928c6c183d0d902724f7
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-08-23 17:11:55 -0400

    messed it up so much i dont know what is wrong so i am deleating the js and starting it over

--- assets/script.js
+++ assets/script.js
@@ -3,48 +3,54 @@ var quizEm = document.querySelector("#quiz");
 var endEm = document.querySelector("#end");
 var startBtn = document.querySelector("#start button");
 var submitScoreBtn = document.querySelector("#start button");
-var quizTitle = document.querySelector("#quiz title");
+var quizTitle = document.querySelector("#quiz #title");
 var questionEm = document.querySelector("#questions");
-var questions = ["First", "second", "third", "fourth"];
-var state = ['start', 'quiz', 'end' ];
+var questions = ["#First", "#second", "#third", "#fourth"];
+var state = 'start';
 var cursor = 0;
-var element = event.target;
 
 function displayState() {
-    if( state == 'start' ) {
+    if ( state === "start" ) {
         startEm.style.display = 'block';
         quizEm.style.display = 'none';
         endEm.style.display = 'none';
     }
-}
-
-function displayState() {
-    if( state == 'quiz' ) {
+    if ( state === "quiz" ) {
         startEm.style.display = 'none';
         quizEm.style.display = 'block';
         endEm.style.display = 'none';
         displayQuestion();
     }
-}
-
-function displayState() {
-    if( state == 'end' ) {
+    if ( state === "end" ) {
         startEm.style.display = 'none';
         quizEm.style.display = 'none';
         endEm.style.display = 'block';
     }
+    debugger;
 }
+    function init() {
+        displayState();
+    }
 
+    function reset() {
+        state= 'start';
+        cursor = 0;
+        init();
+    }
+    
 quizTitle.addEventListener("click", function
 (event) {
+    var element = event.target;
     if (element.matches("h2")) {
-        var index =array.from(element.parrentElement.children).indexOf(element);
+        var index = Array.from(element.parrentElement.children)
+        .indexOf(element);
         console.log(index);
-        cursor ++ ;
+        cursor++;
     if (cursor >= questions.length) {
         state = "end"; 
         displayState();
     }   else {
         displayquestion();
     }}
+    debugger;
 });
